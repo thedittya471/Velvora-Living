aa091c2e72f5010c2df41ba504a5b4132242874a
thedittya471 <dittyamaity@gmail.com>
2026-03-03 19:14:35 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+import connectDb from './db/index.js';
+import { app } from './app.js';
+
+dotenv.config({
+    path: './.env',
+});
+
+connectDb()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running at port ${process.env.PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log('Failed to connect to DB', error);
+        process.exit(1);
+    });
