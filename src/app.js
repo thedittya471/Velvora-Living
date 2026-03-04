c94bbc7ea9801ef5168fb0368c57bc15a3966ca2
thedittya471 <dittyamaity@gmail.com>
2026-03-04 20:05:03 +0530

    added user registration route and controller

--- src/app.js
+++ src/app.js
@@ -16,4 +16,9 @@ app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
 app.use(cookieParser());
 
+// routes
+import userRoutes from './routes/user.routes.js';
+
+app.use('/api/v1/users', userRoutes);
+
 export { app };
