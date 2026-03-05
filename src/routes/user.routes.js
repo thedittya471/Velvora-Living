4cab9b27d7d1487c36764a77cf0a8ddfbde917a5
thedittya471 <dittyamaity@gmail.com>
2026-03-05 22:55:03 +0530

    added logout functionality and JWT verification middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
-import { registerUser, loginUser } from '../controllers/user.controller.js';
+import { registerUser, loginUser, logOutUser } from '../controllers/user.controller.js';
+import { jwtVerify } from '../middlewares/auth.middleware.js';
 
 const router = Router();
 
-router.route('/register').post(registerUser)
-router.route('/login').post(loginUser) // TODO: Implement login route
+router.route('/register').post(registerUser);
+router.route('/login').post(loginUser);
+
+// secured routes
+
+router.route('/logout').post(jwtVerify, logOutUser);
 
 export default router;
