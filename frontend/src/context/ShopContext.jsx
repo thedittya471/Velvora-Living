694b247a4f43d4941f6fc76fa230212b857dde32
thedittya471 <dittyamaity@gmail.com>
2026-03-10 11:37:11 +0530

    add ShopContext for product management; integrate axios for API calls

---
+++ frontend/src/context/ShopContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useState, useEffect } from "react";
+import axios from 'axios'
+
+export const ShopContext = createContext();
+
+const ShopContextProvider = (props) => {
+
+    const currency = '₨'
+    const delivery_fee = 60;
+
+
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                setLoading(true);
+                const response = await axios.get('http://localhost:8000/api/v1/products/get-products')
+                setProducts(response.data.data.products);
+            } catch (error) {
+                setError(error.message);
+                console.error("Error fetching products", error)
+            }
+            finally {
+                setLoading(false)
+            }
+        }
+        fetchProducts()
+    }, [])
+
+    const value = {
+        products,
+        loading,
+        error
+    }
+
+    return (
+        <ShopContext.Provider value={value}>
+            {props.children}
+        </ShopContext.Provider>
+    )
+}
+
+export default ShopContextProvider;
